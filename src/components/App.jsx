3fd2dffb8db3bc2660f7f992f51984f63b2aa990
Jon Gauer <jongauer@Jons-MBP.hsd1.co.comcast.net>
2022-07-07 23:14:42 -0600

    Refactored App.jsx into a functional component with hooks.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,129 +1,140 @@
-import React from "react";
+import React, {useState} from "react";
 import "../App.css";
 import BreakSelector from "./BreakSelector.jsx"
 import WorkSelector from "./WorkSelector.jsx"
 import Timer from "./Timer.jsx"
 import "../App.css"
 
-class App extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            workLength: 25,
-            breakLength: 5,
-            timerMinute: 25,
-            selectorsShouldAppear: true
-        };
+function App(){
+    const [workLength, setWorkLength] = useState(25);
+    const [breakLength, setBreakLength] = useState(5);
+    const [timerMinute, setTimerMinute] = useState(25);
+    const [selectorVisibility, setSelectorVisibility] = useState(true);
+    // constructor(){
+    //     super();
+    //     this.state = {
+    //         workLength: 25,
+    //         breakLength: 5,
+    //         timerMinute: 25,
+    //         selectorsShouldAppear: true
+    //     };
 
-        this.increaseBreakLength = this.increaseBreakLength.bind(this);
-        this.decreaseBreakLength = this.decreaseBreakLength.bind(this);
-        this.increaseWorkLength = this.increaseWorkLength.bind(this);
-        this.decreaseWorkLength = this.decreaseWorkLength.bind(this);
-        this.decrementTimerMinute = this.decrementTimerMinute.bind(this);
-        this.handleWhetherSelectorsShouldAppear = this.handleWhetherSelectorsShouldAppear.bind(this);
-        this.resetTimerMinute = this.resetTimerMinute.bind(this);
-        this.toggleTimerSource = this.toggleTimerSource.bind(this);
+    //     this.increaseBreakLength = this.increaseBreakLength.bind(this);
+    //     this.decreaseBreakLength = this.decreaseBreakLength.bind(this);
+    //     this.increaseWorkLength = this.increaseWorkLength.bind(this);
+    //     this.decreaseWorkLength = this.decreaseWorkLength.bind(this);
+    //     this.decrementTimerMinute = this.decrementTimerMinute.bind(this);
+    //     this.handleWhetherSelectorsShouldAppear = this.handleWhetherSelectorsShouldAppear.bind(this);
+    //     this.resetTimerMinute = this.resetTimerMinute.bind(this);
+    //     this.toggleTimerSource = this.toggleTimerSource.bind(this);
+    // }
 
 
-
-
-
+    function increaseBreakLength() {
+        setBreakLength(breakLength + 1);
+        // this.setState((prevState)=>{
+        //     return{
+        //         breakLength: prevState.breakLength + 1
+        //     }
+        // })
     }
 
-    increaseBreakLength() {
-        this.setState((prevState)=>{
-            return{
-                breakLength: prevState.breakLength + 1
-            }
-        })
+    function decreaseBreakLength() {
+        setBreakLength(breakLength - 1);
+        // this.setState((prevState)=>{
+        //     return {
+        //         breakLength: prevState.breakLength - 1
+        //     }
+        // })
     }
 
-    decreaseBreakLength() {
-        this.setState((prevState)=>{
-            return {
-                breakLength: prevState.breakLength - 1
-            }
-        })
+    function increaseWorkLength() {
+        setWorkLength(workLength + 1);
+        setTimerMinute(timerMinute + 1);
+        // this.setState((prevState)=> {
+        //     return {
+        //         workLength: prevState.workLength + 1,
+        //         timerMinute: prevState.workLength + 1
+        //     }
+        // })
     }
 
-    increaseWorkLength() {
-        this.setState((prevState)=> {
-            return {
-                workLength: prevState.workLength + 1,
-                timerMinute: prevState.workLength + 1
-            }
-        })
+    function decreaseWorkLength() {
+        setWorkLength(workLength - 1);
+        setTimerMinute(timerMinute - 1);
+        // this.setState((prevState)=> {
+        //     return {
+        //         workLength: prevState.workLength - 1,
+        //         timerMinute: prevState.workLength - 1
+        //     }
+        // })
     }
 
-    decreaseWorkLength() {
-        this.setState((prevState)=> {
-            return {
-                workLength: prevState.workLength - 1,
-                timerMinute: prevState.workLength - 1
-            }
-        })
+    function decrementTimerMinute() {
+        setTimerMinute(timerMinute - 1);
+        // this.setState((prevState) => {
+        //     return {
+        //         timerMinute: prevState.timerMinute - 1
+        //     }
+        // })
     }
 
-    decrementTimerMinute() {
-        this.setState((prevState) => {
-            return {
-                timerMinute: prevState.timerMinute - 1
-            }
-        })
-    }
-
-    resetTimerMinute() {
-        this.setState({
-            timerMinute: this.state.workLength
-        })
+    function resetTimerMinute() {
+        setTimerMinute(workLength);
+        // this.setState({
+        //     timerMinute: this.state.workLength
+        // })
     }
 
 
-    toggleTimerSource(isWorking) {
+    function toggleTimerSource(isWorking) {
         if (isWorking) {
-            this.setState({
-                timerMinute: this.state.workLength
-            })
+            setTimerMinute(workLength);
+            // this.setState({
+            //     timerMinute: this.state.workLength
+            // })
         } else {
-            this.setState({
-                timerMinute: this.state.breakLength
-            })
+            setTimerMinute(breakLength);
+            // this.setState({
+            //     timerMinute: this.state.breakLength
+            // })
         }
     }
 
-    handleWhetherSelectorsShouldAppear(status) {
-        this.setState({
-            selectorsShouldAppear: status
-        })
+    function handleWhetherSelectorsShouldAppear(status) {
+        setSelectorVisibility(status)
+        // this.setState({
+        //     selectorsShouldAppear: status
+        // })
     }
 
 
-    render(){
+    
         return (<div className="App">
         <h1>Work/Break Timer</h1>
-        <Timer timerMinute={this.state.timerMinute}
-                breakLength={this.state.breakLength}
-                decrementTimerMinute={this.decrementTimerMinute}
-                toggleTimerSource={this.toggleTimerSource}
-                resetTimerMinute={this.resetTimerMinute}
-                handleWhetherSelectorsShouldAppear={this.handleWhetherSelectorsShouldAppear}
+        <Timer timerMinute={timerMinute}
+                breakLength={breakLength}
+                decrementTimerMinute={decrementTimerMinute}
+                toggleTimerSource={toggleTimerSource}
+                resetTimerMinute={resetTimerMinute}
+                handleWhetherSelectorsShouldAppear={handleWhetherSelectorsShouldAppear}
         />
        <div className="selector-area">
         <BreakSelector 
-        breakLength={this.state.breakLength}
-            increaseBreakLength={this.increaseBreakLength}
-            decreaseBreakLength={this.decreaseBreakLength}
-            selectorsShouldAppear={this.state.selectorsShouldAppear}
+        breakLength={breakLength}
+            increaseBreakLength={increaseBreakLength}
+            decreaseBreakLength={decreaseBreakLength}
+            selectorsShouldAppear={selectorVisibility}
         />
         <WorkSelector
-        workLength={this.state.workLength}
-            increaseWorkLength={this.increaseWorkLength}
-            decreaseWorkLength={this.decreaseWorkLength}
-            selectorsShouldAppear={this.state.selectorsShouldAppear}
+        workLength={workLength}
+            increaseWorkLength={increaseWorkLength}
+            decreaseWorkLength={decreaseWorkLength}
+            selectorsShouldAppear={selectorVisibility}
         />
         </div>
     </div>)
-    }
+    
    
 }
 
